6c86e8fe8fe8dd1e65de9bb79dbaf4d3dd58f2d3
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-03-06 19:42:31 -0300

    feat: implements security settings

--- src/main/java/br/com/calculo_de_impostos/infra/security/SecurityConfig.java
+++ src/main/java/br/com/calculo_de_impostos/infra/security/SecurityConfig.java
@@ -1,4 +1,34 @@
 package br.com.calculo_de_impostos.infra.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request ->
+                request.requestMatchers(HttpMethod.POST, "/tipos").hasRole("ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/cálculo").hasRole("ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/tipos/id").hasRole("ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/usuários/registrar").hasRole("ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/usuários/login").hasRole("ROLE_ADMIN")
+                        .anyRequest().authenticated()
+        );
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
