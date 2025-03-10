110daf1dd76ddabb0507f5428ab1b9c4da77ef4b
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-03-10 15:26:50 -0300

    feat: create tax repository interface

--- src/main/java/br/com/calculo_de_impostos/repositories/TaxRepository.java
+++ src/main/java/br/com/calculo_de_impostos/repositories/TaxRepository.java
@@ -1,4 +1,7 @@
 package br.com.calculo_de_impostos.repositories;
 
+import br.com.calculo_de_impostos.dtos.TaxResponseDto;
+
 public interface TaxRepository {
+    TaxResponseDto saveTax(TaxResponseDto taxToCreate);
 }
