17b3a0126034ea1dff8390e3619cac5c3d2ddbb7
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-03-10 15:27:52 -0300

    feat: implements tax repository interface and add savetax method

---
+++ src/main/java/br/com/calculo_de_impostos/repositories/TaxRepositoryImpl.java
@@ -0,0 +1,20 @@
+package br.com.calculo_de_impostos.repositories;
+
+import br.com.calculo_de_impostos.dtos.TaxResponseDto;
+import br.com.calculo_de_impostos.models.TaxModel;
+
+public class TaxRepositoryImpl implements TaxRepository {
+    private DatabaseTaxRepository databaseTaxRepository;
+
+    public TaxRepositoryImpl (DatabaseTaxRepository databaseTaxRepository){
+        this.databaseTaxRepository = databaseTaxRepository;
+    }
+
+    @Override
+    public TaxResponseDto saveTax(TaxResponseDto taxToCreate) {
+        TaxModel taxToSave = new TaxModel(taxToCreate.getName(), taxToCreate.getDescription(), taxToCreate.getAliquot());
+        TaxModel taxSaved = databaseTaxRepository.save(taxToSave);
+
+        return new TaxResponseDto(taxSaved.getId(), taxSaved.getName(), taxSaved.getDescription(), taxSaved.getAliquot());
+    }
+}
