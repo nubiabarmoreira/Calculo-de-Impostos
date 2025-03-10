71f6029ecef3fe5612329e8e1b90fd0780e4e967
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-03-10 15:25:29 -0300

    feat: implements tax service interface and add createtax method

---
+++ src/main/java/br/com/calculo_de_impostos/services/TaxServiceImpl.java
@@ -0,0 +1,22 @@
+package br.com.calculo_de_impostos.services;
+
+import br.com.calculo_de_impostos.dtos.TaxResponseDto;
+import br.com.calculo_de_impostos.repositories.TaxRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaxServiceImpl implements TaxService {
+    private TaxRepository taxRepository;
+
+    public TaxServiceImpl(TaxRepository taxRepository){
+        this.taxRepository = taxRepository;
+    }
+
+    @Override
+    public TaxResponseDto createTax(String name, String description, double aliquot) {
+        TaxResponseDto taxToCreate = new TaxResponseDto(name, description, aliquot);
+        TaxResponseDto taxCreated = taxRepository.saveTax(taxToCreate);
+
+        return new TaxResponseDto(taxCreated.getName(), taxCreated.getDescription(), taxCreated.getAliquot());
+    }
+}
