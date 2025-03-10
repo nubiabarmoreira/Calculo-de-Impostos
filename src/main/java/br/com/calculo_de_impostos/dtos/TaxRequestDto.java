8db320a54e8a55734b28212a2b170b3a29ab671a
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-03-10 12:09:27 -0300

    feat: creates tax request dto

--- src/main/java/br/com/calculo_de_impostos/dtos/TaxRequestDto.java
+++ src/main/java/br/com/calculo_de_impostos/dtos/TaxRequestDto.java
@@ -1,4 +1,57 @@
 package br.com.calculo_de_impostos.dtos;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.UuidGenerator;
+
 public class TaxRequestDto {
+    @NotBlank(message = "O nome do imposto deve ser informado.")
+    @Size(min = 2, message = "O nome do imposto deve ter no mínimo 2 caracteres.")
+    @Column(name = "nome do imposto")
+    private String name;
+
+    @NotBlank(message = "A descrição do imposto deve ser informada.")
+    @Size(min = 2, message = "A descrição do imposto deve ter no mínimo 2 caracteres.")
+    @Column(name = "descrição do imposto")
+    private String description;
+
+    @Positive(message = "A alíquota do imposto deve ser maior do que zero.")
+    @Column(name = "alíquota")
+    private double aliquot;
+
+    public TaxRequestDto(String name, String description, double aliquot) {
+        this.name = name;
+        this.description = description;
+        this.aliquot = aliquot;
+    }
+
+    public TaxRequestDto() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAliquot() {
+        return aliquot;
+    }
+
+    public void setAliquot(double aliquot) {
+        this.aliquot = aliquot;
+    }
 }
