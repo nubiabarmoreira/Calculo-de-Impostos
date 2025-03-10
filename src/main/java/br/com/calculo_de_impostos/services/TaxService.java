41c6218ee97da119b190df6681eeb38b4f09003d
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-03-10 15:24:21 -0300

    feat: create tax service interface

--- src/main/java/br/com/calculo_de_impostos/services/TaxService.java
+++ src/main/java/br/com/calculo_de_impostos/services/TaxService.java
@@ -1,4 +1,7 @@
 package br.com.calculo_de_impostos.services;
 
+import br.com.calculo_de_impostos.dtos.TaxResponseDto;
+
 public interface TaxService {
+    TaxResponseDto createTax(String name, String description, double aliquot);
 }
