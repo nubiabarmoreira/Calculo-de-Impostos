ff4ba3a64228b39979617d0547f550d0865fa1ff
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-03-12 16:55:15 -0300

    chore: reduces interfaces in the repository layer

--- src/main/java/br/com/calculo_de_impostos/services/taxService/DeleteTaxByIdServiceImpl.java
+++ src/main/java/br/com/calculo_de_impostos/services/taxService/DeleteTaxByIdServiceImpl.java
@@ -1,4 +1,24 @@
 package br.com.calculo_de_impostos.services.taxService;
 
-public class DeleteTaxByIdServiceImpl {
+import br.com.calculo_de_impostos.repositories.taxRepository.DeleteTaxByIdRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeleteTaxByIdServiceImpl implements DeleteTaxByIdService {
+    @Autowired
+    private final DeleteTaxByIdRepository deleteTaxByIdRepository;
+
+    public DeleteTaxByIdServiceImpl(DeleteTaxByIdRepository deleteTaxByIdRepository){
+        this.deleteTaxByIdRepository = deleteTaxByIdRepository;
+    }
+
+    @Override
+    public void deleteTaxById(long id) {
+        if (!deleteTaxByIdRepository.existsById(id)) {
+            throw new EntityNotFoundException("O imposto com o ID " + id + " não foi encontrado.");
+        }
+        deleteTaxByIdRepository.deleteById(id);
+    }
 }
