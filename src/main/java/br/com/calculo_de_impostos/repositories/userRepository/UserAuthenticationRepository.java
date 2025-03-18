f215c52651b5345144ac35d911004d9f9b782fbb
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-03-18 11:13:29 -0300

    feat: implements findPassword method

--- src/main/java/br/com/calculo_de_impostos/repositories/userRepository/UserAuthenticationRepository.java
+++ src/main/java/br/com/calculo_de_impostos/repositories/userRepository/UserAuthenticationRepository.java
@@ -1,12 +1,13 @@
-//package br.com.calculo_de_impostos.repositories.userRepository;
-//
-//import br.com.calculo_de_impostos.models.UserModel;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.Optional;
-//
-//@Repository
-//public interface UserAuthenticationRepository extends JpaRepository<UserModel, Long> {
-//    Optional<UserModel> findByUsername(String username);
-//}
+package br.com.calculo_de_impostos.repositories.userRepository;
+
+import br.com.calculo_de_impostos.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserAuthenticationRepository extends JpaRepository<UserModel, Long> {
+    Optional<UserModel> findByUsername(String username);
+    Optional<UserModel> findPassword(String password);
+}
