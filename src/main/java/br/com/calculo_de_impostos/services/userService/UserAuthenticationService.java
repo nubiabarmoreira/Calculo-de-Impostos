240d1401a01f4be1caee9066804935785614bae5
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-03-18 10:57:52 -0300

    feat: changes method name from authenticateUser to loginAndGenerateToken

--- src/main/java/br/com/calculo_de_impostos/services/userService/UserAuthenticationService.java
+++ src/main/java/br/com/calculo_de_impostos/services/userService/UserAuthenticationService.java
@@ -1,9 +1,8 @@
-//package br.com.calculo_de_impostos.services.userService;
-//
-//import br.com.calculo_de_impostos.dtos.userDtos.UserAuthenticationRequestDto;
-//import br.com.calculo_de_impostos.dtos.userDtos.UserAuthenticationResponseDto;
-//import jakarta.validation.Valid;
-//
-//public interface UserAuthenticationService {
-//    UserAuthenticationResponseDto authenticateUser(@Valid UserAuthenticationRequestDto userAuthenticationRequest);
-//}
+package br.com.calculo_de_impostos.services.userService;
+
+import br.com.calculo_de_impostos.dtos.userDtos.UserAuthenticationRequestDto;
+import jakarta.validation.Valid;
+
+public interface UserAuthenticationService {
+    String loginAndGenerateToken(@Valid UserAuthenticationRequestDto userAuthenticationRequest);
+}
