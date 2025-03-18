c121b5410de929f3646ed10979e21daa407d0744
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-03-18 11:14:54 -0300

    feat: implements token generation

--- src/main/java/br/com/calculo_de_impostos/controllers/userController/UserAuthenticationController.java
+++ src/main/java/br/com/calculo_de_impostos/controllers/userController/UserAuthenticationController.java
@@ -1,29 +1,33 @@
-//package br.com.calculo_de_impostos.controllers.userController;
-//
-//import br.com.calculo_de_impostos.dtos.userDtos.UserAuthenticationRequestDto;
-//import br.com.calculo_de_impostos.dtos.userDtos.UserAuthenticationResponseDto;
-//import br.com.calculo_de_impostos.services.userService.UserAuthenticationService;
-//import jakarta.validation.Valid;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/usuários")
-//public class UserAuthenticationController {
-//    @Autowired
-//    private final UserAuthenticationService userAuthenticationService;
-//
-//    public UserAuthenticationController(UserAuthenticationService userAuthenticationService) {
-//        this.userAuthenticationService = userAuthenticationService;
-//    }
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<UserAuthenticationResponseDto> authenticateUser(@Valid @RequestBody UserAuthenticationRequestDto userAuthenticationRequest) {
-//        UserAuthenticationResponseDto userAuthenticationResponse = userAuthenticationService.authenticateUser(userAuthenticationRequest);
-//        return ResponseEntity.ok(userAuthenticationResponse);
-//    }
-//}
+package br.com.calculo_de_impostos.controllers.userController;
+
+import br.com.calculo_de_impostos.dtos.userDtos.UserAuthenticationRequestDto;
+import br.com.calculo_de_impostos.dtos.userDtos.UserAuthenticationResponseDto;
+import br.com.calculo_de_impostos.services.userService.UserAuthenticationService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuários")
+public class UserAuthenticationController {
+    @Autowired
+    private final UserAuthenticationService userAuthenticationService;
+
+    public UserAuthenticationController(UserAuthenticationService userAuthenticationService) {
+        this.userAuthenticationService = userAuthenticationService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserAuthenticationResponseDto> authenticateUser(@Valid @RequestBody UserAuthenticationRequestDto userAuthenticationRequest) {
+        String token = userAuthenticationService.loginAndGenerateToken(userAuthenticationRequest);
+
+        UserAuthenticationResponseDto userAuthenticationResponse = new UserAuthenticationResponseDto();
+        userAuthenticationResponse.setToken(token);
+
+        return ResponseEntity.ok().body(userAuthenticationResponse);
+    }
+}
