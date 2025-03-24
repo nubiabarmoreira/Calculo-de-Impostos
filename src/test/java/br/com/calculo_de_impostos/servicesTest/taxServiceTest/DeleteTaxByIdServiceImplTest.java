c9fe94dfefac1678a5dde8604a440f26463289b7
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-03-24 17:10:21 -0300

    test: create unit test testDeleteTaxByIdFail

--- src/test/java/br/com/calculo_de_impostos/servicesTest/taxServiceTest/DeleteTaxByIdServiceImplTest.java
+++ src/test/java/br/com/calculo_de_impostos/servicesTest/taxServiceTest/DeleteTaxByIdServiceImplTest.java
@@ -3,6 +3,8 @@ package br.com.calculo_de_impostos.servicesTest.taxServiceTest;
 import br.com.calculo_de_impostos.models.TaxModel;
 import br.com.calculo_de_impostos.repositories.taxRepository.DeleteTaxByIdRepository;
 import br.com.calculo_de_impostos.services.taxService.DeleteTaxByIdService;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -27,12 +29,26 @@ public class DeleteTaxByIdServiceImplTest {
 
     @Test
     public void testDeleteTaxByIdSuccess() {
-        Long id = 1L;
+        long id = 1L;
 
         Mockito.when(deleteTaxByIdRepository.existsById(id)).thenReturn(true);
 
         deleteTaxByIdService.deleteTaxById(id);
 
         Mockito.verify(deleteTaxByIdRepository, Mockito.times(1)).deleteById(id);
+    }
+
+    @Test
+    public void testDeleteTaxByIdFail() {
+        long invalidId = 2L;
+
+        Mockito.when(deleteTaxByIdRepository.existsById(invalidId)).thenReturn(false);
+
+        EntityNotFoundException entityNotFoundException = Assertions
+                .assertThrows(EntityNotFoundException.class, () -> deleteTaxByIdService.deleteTaxById(invalidId));
+
+        Assertions.assertEquals("Imposto com o ID " + invalidId + " não encontrado.", entityNotFoundException.getMessage());
+
+        Mockito.verify(deleteTaxByIdRepository, Mockito.never()).deleteById(Mockito.anyLong());
     }
 }
