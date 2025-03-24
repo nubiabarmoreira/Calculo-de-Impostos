9a7004e4f32b35981016a6fb406dcdfec351a662
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-03-24 16:19:52 -0300

    test: create unit test testCreateTaxSuccess

--- src/test/java/br/com/calculo_de_impostos/servicesTest/taxServiceTest/CreateTaxServiceImplTest.java
+++ src/test/java/br/com/calculo_de_impostos/servicesTest/taxServiceTest/CreateTaxServiceImplTest.java
@@ -1,4 +1,54 @@
 package br.com.calculo_de_impostos.servicesTest.taxServiceTest;
 
+import br.com.calculo_de_impostos.dtos.taxDtos.TaxCalculationRequestDto;
+import br.com.calculo_de_impostos.dtos.taxDtos.TaxCalculationResponseDto;
+import br.com.calculo_de_impostos.dtos.taxDtos.TaxResponseDto;
+import br.com.calculo_de_impostos.models.TaxModel;
+import br.com.calculo_de_impostos.repositories.taxRepository.SaveTaxRepository;
+import br.com.calculo_de_impostos.services.taxService.CreateTaxService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class CreateTaxServiceImplTest {
+
+    @Mock
+    private SaveTaxRepository saveTaxRepository;
+
+    @InjectMocks
+    private CreateTaxService createTaxService;
+
+    TaxModel taxModel = new TaxModel();
+    TaxCalculationRequestDto taxCalculationRequest = new TaxCalculationRequestDto();
+
+    @Test
+    public void testCreateTaxSuccess() {
+        String name = "ICMS";
+        String description = "ICMS";
+        double aliquot = 18.0;
+
+        TaxModel taxToCreate = new TaxModel(name, description, aliquot);
+        TaxModel taxCreated = new TaxModel(name, description, aliquot);
+        taxCreated.setId(1L);
+
+        Mockito.when(saveTaxRepository.save(taxToCreate)).thenReturn(taxCreated);
+
+        TaxResponseDto taxResponse = createTaxService.createTax(name, description, aliquot);
+
+        Assertions.assertEquals("ICMS", taxResponse.getName());
+        Assertions.assertEquals("ICMS", taxResponse.getDescription());
+        Assertions.assertEquals(18.0, taxResponse.getAliquot());
+    }
+
+
+
 }
