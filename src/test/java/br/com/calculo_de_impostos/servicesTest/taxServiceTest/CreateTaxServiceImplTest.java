a442d31715e22766542c24ef4fb987c9b5e627e7
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-03-25 10:38:48 -0300

    test: fix unit test testCreateTaxSuccess method

--- src/test/java/br/com/calculo_de_impostos/servicesTest/taxServiceTest/CreateTaxServiceImplTest.java
+++ src/test/java/br/com/calculo_de_impostos/servicesTest/taxServiceTest/CreateTaxServiceImplTest.java
@@ -1,34 +1,34 @@
 package br.com.calculo_de_impostos.servicesTest.taxServiceTest;
 
-import br.com.calculo_de_impostos.dtos.taxDtos.TaxCalculationRequestDto;
-import br.com.calculo_de_impostos.dtos.taxDtos.TaxCalculationResponseDto;
 import br.com.calculo_de_impostos.dtos.taxDtos.TaxResponseDto;
 import br.com.calculo_de_impostos.models.TaxModel;
 import br.com.calculo_de_impostos.repositories.taxRepository.SaveTaxRepository;
-import br.com.calculo_de_impostos.services.taxService.CreateTaxService;
+import br.com.calculo_de_impostos.services.taxService.CreateTaxServiceImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.MockitoAnnotations;
 
-import java.util.Optional;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
-@ExtendWith(MockitoExtension.class)
+//@SpringBootTest
+//@ExtendWith(MockitoExtension.class)
 public class CreateTaxServiceImplTest {
 
     @Mock
     private SaveTaxRepository saveTaxRepository;
 
     @InjectMocks
-    private CreateTaxService createTaxService;
+    private CreateTaxServiceImpl createTaxService;
 
-    TaxModel taxModel = new TaxModel();
-    TaxCalculationRequestDto taxCalculationRequest = new TaxCalculationRequestDto();
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
 
     @Test
     public void testCreateTaxSuccess() {
@@ -36,11 +36,10 @@ public class CreateTaxServiceImplTest {
         String description = "ICMS";
         double aliquot = 18.0;
 
-        TaxModel taxToCreate = new TaxModel(name, description, aliquot);
         TaxModel taxCreated = new TaxModel(name, description, aliquot);
         taxCreated.setId(1L);
 
-        Mockito.when(saveTaxRepository.save(taxToCreate)).thenReturn(taxCreated);
+        when(saveTaxRepository.save(any(TaxModel.class))).thenReturn(taxCreated);
 
         TaxResponseDto taxResponse = createTaxService.createTax(name, description, aliquot);
 
@@ -81,9 +80,13 @@ public class CreateTaxServiceImplTest {
         String description = "ICMS";
         double aliquot = -5.0;
 
-        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            createTaxService.createTax(name, description, aliquot);
-        });
+        TaxModel taxToCreate = new TaxModel(name, description, aliquot);
+        TaxModel taxCreated = new TaxModel(name, description, aliquot);
+
+        when(saveTaxRepository.save(taxToCreate)).thenReturn(taxCreated);
+
+        IllegalArgumentException exception = Assertions
+                .assertThrows(IllegalArgumentException.class, () -> createTaxService.createTax(name, description, aliquot));
 
         Assertions.assertEquals("A alíquota do imposto deve ser maior do que zero.", exception.getMessage());
     }
